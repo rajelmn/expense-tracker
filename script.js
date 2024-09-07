2ba08ce137acce940e0413d0417487b8d80dfecf
brahim <rajelghmn@gmail.com>
2024-09-07 08:54:48 +0000

    adding the remove logic , and keeping everything working sync with localStorage

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { generateId } from "./unique.js";
 const btn = document.getElementById('btn');
 const table = document.querySelector('table');
 const dateInput = document.querySelector('.second-input .date input');
@@ -8,14 +9,29 @@ const dropDownInput = document.getElementById('drop-down');
 const defaultValue = document.getElementById('noExpense') ;
 let total = document.getElementById('total');
 
-// const request = window.indexedDB.open('test', 3);
+// function that will be used when deleting
 
-// request.onsuccess = (e) => {
-//     console.log(e)
-// }
+function handleDelete(remove, id) {
+    remove.addEventListener('click', () => {
+        let sum = 0;
+        remove.parentElement.style.display = 'none';
+        const test = JSON.parse(localStorage.getItem('items'));
+        const filteredItems = test.filter((item) => item.id !== id);
+
+        for(let item of filteredItems) {
+            sum += (+item.price || +item.salary || +item.other)
+        }
+
+        
+        localStorage.setItem('items', JSON.stringify(filteredItems));
+        localStorage.setItem('total', sum)
+        total.textContent = `Total = ${localStorage.getItem('total')} MRU`
+       })
+}
 
 btn.addEventListener('click', () => {
     let sum = 0; 
+    const uniqueId = Math.random();
     if(dateInput.value && amountInput.value && textInput.value ) {
         defaultValue ? defaultValue.style.display = 'none': '';
         let tr = document.createElement('tr');
@@ -26,6 +42,11 @@ btn.addEventListener('click', () => {
         let td4 = document.createElement('td');
         let td5 = document.createElement('td');
         let td6 = document.createElement('td');
+        const id = generateId();
+        const remove = document.createElement('td');
+        remove.textContent = 'X';
+        remove.className = 'remove';
+
 
         switch(selectOptions.value) {
             case 'prix':
@@ -37,10 +58,9 @@ btn.addEventListener('click', () => {
             case 'salaire': 
             td5.innerHTML = dropDownInput.value
         }
-        // let remove = document.createElement('td');
-        // remove.textContent = 'X'
-        // remove.className = 'remove'
-        table.appendChild(tr).append(td1, td2 , td3, td4, td5, td6)
+        
+        handleDelete(remove, id, sum);
+        table.appendChild(tr).append(td2, td1 , td3, td4, td5, td6, remove)
         td1.innerHTML = textInput.value;
         td2.innerHTML = dateInput.value;
         td3.innerHTML = amountInput.value;
@@ -53,6 +73,8 @@ btn.addEventListener('click', () => {
             price: td4.textContent,
             salary: td5.textContent,
             other: td6.textContent,
+            remove: remove.textContent,
+            id,
         })
         
         if(localStorage.length) {
@@ -66,7 +88,7 @@ btn.addEventListener('click', () => {
         sum += (+expense.price || +expense.salary || +expense.other ) 
         if(localStorage.getItem('total')) {
             sum += Number(localStorage.getItem('total'))
-            total.textContent = `${sum} MRU`;
+            total.textContent = `Total = ${sum} MRU`;
             localStorage.setItem('total', sum);
             return
         }
@@ -96,14 +118,18 @@ window.addEventListener('load', () => {
         let price = document.createElement('td')
         let salary = document.createElement('td')
         let other = document.createElement('td');
-
+        let remove = document.createElement('td');
+        let id = item.id;
         name.textContent = item.name;
         qty.textContent = item.qty;
         purchaseDate.textContent = item.purchaseDate;
         price.textContent = item.price;
         salary.textContent = item.salary;
         other.textContent = item.other;
-        table.appendChild(tr).append(name, purchaseDate, qty, price, salary, other)
+        remove.textContent = item.remove;
+        remove.className = 'remove';
+        handleDelete(remove, id);
+        table.appendChild(tr).append(purchaseDate, name, qty, price, salary, other, remove)
     }
     total.textContent = `total = ${localStorage.getItem('total')} MRU`
 })
